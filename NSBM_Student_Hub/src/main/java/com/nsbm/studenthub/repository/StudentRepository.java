555aee6730cfd77f6a8c07da9b367db6e6a7e355
himeshishavindi <khshavindi@students.nsbm.ac.lk>
2026-02-09 16:27:21 +0530

    delete

--- NSBM_Student_Hub/src/main/java/com/nsbm/studenthub/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nsbm.studenthub.repository;
-
-import com.nsbm.studenthub.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
